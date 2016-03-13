7f6805b41d49c49173e014eef3454cfb57f8f8b9
towen <towenpa@gmail.com>
2016-03-12 23:51:31 -0500

    Comenzando trabajo en el ACP

--- acp/feeds_bot_module.php
+++ acp/feeds_bot_module.php
@@ -63,93 +63,41 @@ class feeds_bot_module
         switch($mode)
         {
             case 'list':
+                $this->tpl_name = 'acp_feeds_bot';
                 $action = $this->request->variable('action', '');
+                $this->page_title = $this->user->lang['ACP_FEEDS_BOT_LIST'];
+
+                if ($action == 'edit' || $action == 'delete')
+                {
+                    $feed_id = $this->request->variable('feed_id', 0);
+
+                    if (!$feed_id)
+                    {
+                        trigger_error($this->user->lang['NO_FEED_ID'] . adm_back_link($this->u_action), E_USER_WARNING);
+                    }
+
+                    $this->page_title = $this->user->lang['ACP_FEEDS_BOT_'.strtoupper($action)];
+                }
 
                 switch($action)
                 {
                     case 'edit':
-                        $feed_id = $this->request->variable('feed_id', 0);
 
-                        if (!$feed_id)
-                        {
-                            $message = $this->user->lang['NO_FEED_ID'];
-                            $message_type = E_USER_WARNING;
+                    // no break;
 
-                            trigger_error($message . adm_back_link($this->u_action), $message_type);
-                        }
+                    case 'add':
 
-                        $this->tpl_name = 'acp_board';
-                        $this->page_title = $this->user->lang[$display_vars['title']];
-
-                        $display_vars = array(
-                            'title'	=> 'ACP_FEEDS_BOT_CONFIG',
-                            'vars'	=> array(
-                                'legend1'				=> 'GENERAL_SETTINGS',
-                                'enabled'   			=> array('lang' => 'ENABLE_EMAIL',			'validate' => 'bool',	'type' => 'radio:enabled_disabled', 'explain' => true),
-                                'board_email_form'		=> array('lang' => 'BOARD_EMAIL_FORM',		'validate' => 'bool',	'type' => 'radio:enabled_disabled', 'explain' => true),
-                                'email_function_name'	=> array('lang' => 'EMAIL_FUNCTION_NAME',	'validate' => 'string',	'type' => 'text:20:50', 'explain' => true),
-                                'email_package_size'	=> array('lang' => 'EMAIL_PACKAGE_SIZE',	'validate' => 'int:0',	'type' => 'number:0:99999', 'explain' => true),
-                                'board_contact'			=> array('lang' => 'CONTACT_EMAIL',			'validate' => 'email',	'type' => 'email:25:100', 'explain' => true),
-                                'board_contact_name'	=> array('lang' => 'CONTACT_EMAIL_NAME',	'validate' => 'string',	'type' => 'text:25:50', 'explain' => true),
-                                'board_email'			=> array('lang' => 'ADMIN_EMAIL',			'validate' => 'email',	'type' => 'email:25:100', 'explain' => true),
-                                'board_email_sig'		=> array('lang' => 'EMAIL_SIG',				'validate' => 'string',	'type' => 'textarea:5:30', 'explain' => true),
-                                'board_hide_emails'		=> array('lang' => 'BOARD_HIDE_EMAILS',		'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => true),
-
-                                'legend2'					=> 'ACP_SUBMIT_CHANGES',
-                            )
-                        );
-
-                        $new_config = $this->config;
-                        $submit = $request->is_set_post('submit');
-                        $cfg_array = ($request->is_set_post('config')) ? utf8_normalize_nfc($this->request->variable('config', array('' => ''), true)) : $new_config;
-                        $error = array();
-
-                        validate_config_vars($display_vars['vars'], $cfg_array, $error);
-
-                        if ($submit && !check_form_key($form_key))
-                        {
-                            $error[] = $this->user->lang['FORM_INVALID'];
-                        }
-
-                        // Do not write values if there is an error
-                        if (!sizeof($error) && $submit)
-                        {
-                            foreach ($display_vars['vars'] as $config_name => $null)
-                            {
-                                if (!isset($cfg_array[$config_name]) || strpos($config_name, 'legend') !== false)
-                                {
-                                    continue;
-                                }
-
-                                set_config($config_name, $cfg_array[$config_name]);
-                            }
-
-                            //$this->log->add('admin', 'LOG_FEEDS_BOT_CONFIG_UPDATED');
-
-                            $message = $this->user->lang['CONFIG_UPDATED'];
-                            $message_type = E_USER_NOTICE;
-
-                            trigger_error($message . adm_back_link($this->u_action), $message_type);
-                        }
-
-                        $this->display_config($display_vars, $error, $new_config);
+                        $template->assign_vars(array(
+                            'S_EDIT' => true,
+                        ));
                     break;
 
                     case 'delete':
-                        $feed_id = $this->request->variable('feed_id', 0);
-
-                        if (!$feed_id)
-                        {
-                            $message = $this->user->lang['NO_FEED_ID'];
-                            $message_type = E_USER_WARNING;
-
-                            trigger_error($message . adm_back_link($this->u_action), $message_type);
-                        }
 
                         if (confirm_box(true))
                         {
 
-                            //$this->log.add('admin', 'LOG_FEEDS_BOT_FEED_DELETED');
+                            //$this->log.add('admin', 'LOG_FEEDS_BOT_FEED_DELETED'); TODO
                         }
                         else
                         {
@@ -164,6 +112,7 @@ class feeds_bot_module
                     // case 'list':
                     default:
 
+
                     break;
                 }
 
@@ -171,7 +120,7 @@ class feeds_bot_module
 
             case 'config':
                 $this->tpl_name = 'acp_board';
-                $this->page_title = $this->user->lang[$display_vars['title']];
+                $this->page_title = $this->user->lang['ACP_FEEDS_BOT_CONFIG'];
 
                 $display_vars = array(
                     'title'	=> 'ACP_FEEDS_BOT_CONFIG',
@@ -179,7 +128,7 @@ class feeds_bot_module
                         'legend1'				=> 'GENERAL_SETTINGS',
                         'feeds_bot_enabled'		=> array('lang' => 'FEEDS_BOT_ENABLED',		'validate' => 'bool',	'type' => 'radio:enabled_disabled', 'explain' => true),
                         'feeds_bot_user_agent'	=> array('lang' => 'FEEDS_BOT_USER_AGENT',	'validate' => 'string',	'type' => 'text:50:255', 'explain' => true),
-                        'feeds_bot_gc'	        => array('lang' => 'FEEDS_BOT_UPDATE_TIME',	'validate' => 'int:15',	'type' => 'number:15', 'explain' => true),
+                        'feeds_bot_gc'	        => array('lang' => 'FEEDS_BOT_UPDATE_TIME',	'validate' => 'int:10',	'type' => 'number:10', 'explain' => true),
 
                         'legend2'				=> 'ACP_SUBMIT_CHANGES',
                     )
@@ -214,12 +163,9 @@ class feeds_bot_module
                         set_config($config_name, $cfg_array[$config_name]);
                     }
 
-                    //$this->log->add('admin', 'LOG_FEEDS_BOT_CONFIG_UPDATED');
+                    //$this->log->add('admin', 'LOG_FEEDS_BOT_CONFIG_UPDATED'); // TODO
 
-                    $message = $this->user->lang['CONFIG_UPDATED'];
-                    $message_type = E_USER_NOTICE;
-
-                    trigger_error($message . adm_back_link($this->u_action), $message_type);
+                    trigger_error($this->user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action), E_USER_NOTICE);
                 }
 
                 $this->display_config($display_vars, $error, $new_config);
@@ -269,7 +215,7 @@ class feeds_bot_module
                 $l_explain = (isset($this->user->lang[$vars['lang'] . '_EXPLAIN'])) ? $this->user->lang[$vars['lang'] . '_EXPLAIN'] : '';
             }
 
-            if ($config_key == 'feeds_bot_gc' || $config_key == 'update_interval')
+            if ($config_key == 'feeds_bot_gc')
             {
                 $new_config[$config_key] /= 60;
             }

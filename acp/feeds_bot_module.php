a574941442811dd698006d51736b66ba7a796f4b
towen <towenpa@gmail.com>
2016-03-13 00:39:11 -0500

    Cambiado sistema de configuracion en el ACP

--- acp/feeds_bot_module.php
+++ acp/feeds_bot_module.php
@@ -119,123 +119,51 @@ class feeds_bot_module
             break;
 
             case 'config':
-                $this->tpl_name = 'acp_board';
+                $this->tpl_name = 'acp_feeds_bot_config';
                 $this->page_title = $this->user->lang['ACP_FEEDS_BOT_CONFIG'];
 
-                $display_vars = array(
-                    'title'	=> 'ACP_FEEDS_BOT_CONFIG',
-                    'vars'	=> array(
-                        'legend1'				=> 'GENERAL_SETTINGS',
-                        'feeds_bot_enabled'		=> array('lang' => 'FEEDS_BOT_ENABLED',		'validate' => 'bool',	'type' => 'radio:enabled_disabled', 'explain' => true),
-                        'feeds_bot_user_agent'	=> array('lang' => 'FEEDS_BOT_USER_AGENT',	'validate' => 'string',	'type' => 'text:50:255', 'explain' => true),
-                        'feeds_bot_gc'	        => array('lang' => 'FEEDS_BOT_UPDATE_TIME',	'validate' => 'int:10',	'type' => 'number:10', 'explain' => true),
+                $feeds_bot_enabled = $request->variable('feeds_bot_enabled', (bool)$config['feeds_bot_enabled']);
+                $feeds_bot_user_agent = $request->variable('feeds_bot_user_agent', (string)$config['feeds_bot_user_agent'], true);
+                $feeds_bot_gc = $request->variable('feeds_bot_gc', (int)($config['feeds_bot_gc']/60));
 
-                        'legend2'				=> 'ACP_SUBMIT_CHANGES',
-                    )
-                );
-
-                $new_config = $this->config;
-                $submit = $this->request->is_set_post('submit');
-                $cfg_array = ($this->request->is_set_post('config')) ? utf8_normalize_nfc($this->request->variable('config', array('' => ''), true)) : $new_config;
                 $error = array();
-
-                validate_config_vars($display_vars['vars'], $cfg_array, $error);
+                $submit = $this->request->is_set_post('submit');
 
                 if ($submit && !check_form_key($form_key))
                 {
                     $error[] = $this->user->lang['FORM_INVALID'];
                 }
 
+                if ($feeds_bot_gc < 10)
+                {
+                    $error[] = $this->user->lang['FEEDS_BOT_GC_INVALID'];
+                }
+
                 // Do not write values if there is an error
                 if (!sizeof($error) && $submit)
                 {
-                    foreach ($display_vars['vars'] as $config_name => $null)
-                    {
-                        if (!isset($cfg_array[$config_name]) || strpos($config_name, 'legend') !== false)
-                        {
-                            continue;
-                        }
-
-                        if ($config_name == 'feeds_bot_gc')
-                        {
-                            $cfg_array[$config_name] *= 60;
-                        }
-                        set_config($config_name, $cfg_array[$config_name]);
-                    }
+                    set_config('feeds_bot_enabled', $feeds_bot_enabled);
+                    set_config('feeds_bot_user_agent', $feeds_bot_user_agent);
+                    set_config('feeds_bot_gc', $feeds_bot_gc * 60);
 
                     //$this->log->add('admin', 'LOG_FEEDS_BOT_CONFIG_UPDATED'); // TODO
 
                     trigger_error($this->user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action), E_USER_NOTICE);
                 }
 
-                $this->display_config($display_vars, $error, $new_config);
+                $this->template->assign_vars(array(
+                    'L_TITLE'			=> $this->user->lang['ACP_FEEDS_BOT_CONFIG'],
+
+                    'S_ERROR'			=> (sizeof($error)) ? true : false,
+                    'ERROR_MSG'			=> implode('<br />', $error),
+
+                    'FEEDS_BOT_ENABLED' => $feeds_bot_enabled,
+                    'FEEDS_BOT_USER_AGENT'  => $feeds_bot_user_agent,
+                    'FEEDS_BOT_GC'      => $feeds_bot_gc,
+
+                    'U_ACTION'			=> $this->u_action,
+                ));
             break;
         }
-    }
-
-    private function display_config($display_vars, $error, $new_config)
-    {
-        $this->template->assign_vars(array(
-            'L_TITLE'			=> $this->user->lang[$display_vars['title']],
-            'L_TITLE_EXPLAIN'	=> $this->user->lang[$display_vars['title'] . '_EXPLAIN'],
-
-            'S_ERROR'			=> (sizeof($error)) ? true : false,
-            'ERROR_MSG'			=> implode('<br />', $error),
-
-            'U_ACTION'			=> $this->u_action)
-        );
-
-        // Output relevant page
-        foreach ($display_vars['vars'] as $config_key => $vars)
-        {
-            if (!is_array($vars) && strpos($config_key, 'legend') === false)
-            {
-                continue;
-            }
-
-            if (strpos($config_key, 'legend') !== false)
-            {
-                $this->template->assign_block_vars('options', array(
-                    'S_LEGEND'		=> true,
-                    'LEGEND'		=> (isset($this->user->lang[$vars])) ? $this->user->lang[$vars] : $vars)
-                );
-
-                continue;
-            }
-
-            $type = explode(':', $vars['type']);
-
-            $l_explain = '';
-            if ($vars['explain'] && isset($vars['lang_explain']))
-            {
-                $l_explain = (isset($this->user->lang[$vars['lang_explain']])) ? $this->user->lang[$vars['lang_explain']] : $vars['lang_explain'];
-            }
-            else if ($vars['explain'])
-            {
-                $l_explain = (isset($this->user->lang[$vars['lang'] . '_EXPLAIN'])) ? $this->user->lang[$vars['lang'] . '_EXPLAIN'] : '';
-            }
-
-            if ($config_key == 'feeds_bot_gc')
-            {
-                $new_config[$config_key] /= 60;
-            }
-            $content = build_cfg_template($type, $config_key, $new_config, $config_key, $vars);
-
-            if (empty($content))
-            {
-                continue;
-            }
-
-            $this->template->assign_block_vars('options', array(
-                'KEY'			=> $config_key,
-                'TITLE'			=> (isset($this->user->lang[$vars['lang']])) ? $this->user->lang[$vars['lang']] : $vars['lang'],
-                'S_EXPLAIN'		=> $vars['explain'],
-                'TITLE_EXPLAIN'	=> $l_explain,
-                'CONTENT'		=> $content,
-            ));
-
-            unset($display_vars['vars'][$config_key]);
-        }
-
     }
 }

5731ba3f67925250f1aabe3387324c281c71bb5e
towen <towenpa@gmail.com>
2016-07-26 01:33:18 -0400

    Agregado el cambio de user_agent al feed_loader

--- core/feed_loader.php
+++ core/feed_loader.php
@@ -9,17 +9,22 @@
 
 namespace towen\feeds_bot\core;
 
-/**
- * Class feed_loader
- * @package towen\feeds_bot\core
- */
 class feed_loader {
-	/**
-	 * @param $url
-	 *
-	 * @return \SimpleXMLElement
-	 */
+	protected $config;
+
+	public function __construct(\phpbb\config\config $config)
+	{
+		$this->config = $config;
+	}
+
 	public function getXML($url) {
-		return @simplexml_load_file($url);
+		$old_user_agent = ini_get('user_agent');
+		ini_set("user_agent", $this->config['feeds_bot_user_agent']);
+
+		$xml = @simplexml_load_file($url);
+
+		ini_set("user_agent", $old_user_agent);
+
+		return $xml;
 	}
 }

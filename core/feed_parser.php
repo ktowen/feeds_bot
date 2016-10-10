af4ad03c28413b747ea431aae67492aaef398733
towen <towenpa@gmail.com>
2016-10-10 14:47:09 -0400

    Mejorado el parser

--- core/feed_parser.php
+++ core/feed_parser.php
@@ -9,7 +9,8 @@
 
 namespace towen\feeds_bot\core;
 
-class feed_parser {
+class feed_parser
+{
 
 	protected $config;
 	protected $user;
@@ -30,10 +31,10 @@ class feed_parser {
 		$this->php_ext = $php_ext;
 	}
 
-	public function get_feed_object(\SimpleXMLElement $feed_xml) {
+	public function get_feed_object($feed_xml) {
 		$feed = null;
 
-		foreach ($this->feed_types as $feed)
+		foreach ($this->feed_types as $type)
 		{
 			$type->load_xml($feed_xml);
 			if ($type->is_valid())
@@ -46,17 +47,19 @@ class feed_parser {
 		return $feed;
 	}
 
-	public function parse($entry, $feed, array $feed_config) {
+	public function parse($entry, $feed, $feed_config) {
 		return array(
 			'title'	=> $this->parse_tokens(
 						$feed_config['subject_template'],
 						$feed,
-						$entry
+						$entry,
+						$feed_config
 					),
 			'content'	=> $this->parse_tokens(
 						$feed_config['body_template'],
 						$feed,
-						$entry
+						$entry,
+						$feed_config
 					),
 		);
 	}
@@ -78,29 +81,37 @@ class feed_parser {
 		return $tokens;
 	}
 
-	private function html_to_bbcode(string $text) {
+	private function html_to_bbcode($text) {
 		$html = array(
 			'#<b(?:.*?)>(.*?)</b>#is',
+			'#<span style="font-weight: bold">(.*?)</span>#',
 			'#<strong(?:.*?)>(.*?)</strong>#is',
 			'#<i(?:.*?)>(.*?)</i>#is',
 			'#<em(?:.*?)>(.*?)</em>#is',
+			'#<span style="font-style: italic">(.*?)</span>#',
 			'#<u(?:.*?)>(.*?)</u>#is',
+			'#<span style="text-decoration: underline">(.*?)</span>#',
 			'#<ul(?:.*?)>(.*?)</ul>#is',
 			'#<ol(?:.*?)>(.*?)</ol>#is',
 			'#<li(?:.*?)>(.*?)</li>#is',
 			'#<div(?:.*?)>(.*?)</div>#is',
 			'#<p(?:.*?)>(.*?)</p>#is',
 			'#<span(?:.*?)>(.*?)</span>#is',
-			'#</?br>#is',
+			'#<br\s?/?>#is',
 			'#<img(?:.*?)src="(.*?)"(?:.*?)/?>#is',
 			'#<a(?:.*?)href="(.*?)"(?:.*?)>(.*?)</a>#is',
+			'#<code(?:.*?)>(.*?)</code>#is',
+			'#<blockquote(?:.*?)>(.*?)</blockquote>#is',
 		);
 
 		$bbcode = array(
 			"[b]$1[/b]",
 			"[b]$1[/b]",
+			"[b]$1[/b]",
 			"[i]$1[/i]",
 			"[i]$1[/i]",
+			"[i]$1[/i]",
+			"[u]$1[/u]",
 			"[u]$1[/u]",
 			"[list]$1[/list]",
 			"[list=1]$1[/list]",
@@ -111,14 +122,38 @@ class feed_parser {
 			"\n",
 			"[img]$1[/img]",
 			"[url=$1]$2[/url]",
+			"[code]$1[/code]",
+			"[quote]$1[/quote]",
 		);
 
 		$text = preg_replace($html, $bbcode, $text);
 		return $text;
 	}
 
-	private function prepare_text(string $text, bool $censor) {
-		if ($censor)
+	private function parse_tokens($template, $feed, $entry, $feed_config) {
+		$tokens = $this->get_tokens();
+
+		$text = preg_replace_callback(
+			'#\{([A-Z_]+)\}#',
+			function($matches) use ($tokens, $feed, $entry) {
+				if (!isset($tokens[$matches[1]])) {
+					return $matches[0];
+				}
+				$token_data = $tokens[$matches[1]];
+
+				if (!empty($token_data[1]))
+				{
+					return $entry->{$token_data[0]}();
+				}
+				else
+				{
+					return $feed->{$token_data[0]}();
+				}
+			},
+			$template
+		);
+
+		if ($feed_config['censor_text'])
 		{
 			if (!function_exists('censor_text'))
 			{
@@ -127,33 +162,9 @@ class feed_parser {
 
 			$text = censor_text($text);
 		}
-		$text = strip_tags($text);
-
-		return $text;
-	}
-
-	private function parse_tokens(string $template, \SimpleXMLElement $feed, \SimpleXMLElement $entry) {
-		$tokens = $this->get_tokens();
-
-		$text = preg_replace_callback(
-			array_keys($tokens),
-			function($token) use ($entry, $feed, $tokens) {
-
-				$token_data = $tokens[$token];
-
-				if (!empty($data[1]))
-				{
-					$replace = $entry->${$token_data[0]}();
-				}
-				else
-				{
-					$replace = $feed->${$token_data[0]}();
-				}
-			},
-			$template);
 
 		$text = $this->html_to_bbcode($text);
-		$text = $this->prepare_text($text);
+		$text = strip_tags($text);
 
 		return $text;
 	}
